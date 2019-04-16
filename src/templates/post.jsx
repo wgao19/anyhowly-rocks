df2e4fd6d30be32af47742c283a3dc81031c3d2a
Wei Gao <wgao19@gmail.com>
2019-04-17 02:24:57 +0800

    More layout

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -9,7 +9,7 @@ import SocialLinks from "../components/SocialLinks/SocialLinks";
 import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
 import "./b16-tomorrow-dark.css";
-import "./post.css";
+import s from "./post.module.scss";
 
 export default class PostTemplate extends React.Component {
   render() {
@@ -29,15 +29,15 @@ export default class PostTemplate extends React.Component {
             <title>{`${post.title} | ${config.siteTitle}`}</title>
           </Helmet>
           <SEO postPath={slug} postNode={postNode} postSEO />
-          <div>
+          <article className={s.article}>
             <h1>{post.title}</h1>
             <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
-            <div className="post-meta">
+            <div className={s.postMeta}>
               <PostTags tags={post.tags} />
               <SocialLinks postPath={slug} postNode={postNode} />
             </div>
             <UserInfo config={config} />
-          </div>
+          </article>
         </div>
       </Layout>
     );
