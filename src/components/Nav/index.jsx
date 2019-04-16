69aec8635bcf0d316d87984d5c403e66f4ef81d2
Wei Gao <wgao19@gmail.com>
2019-04-17 00:55:17 +0800

    Basic layout

---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+export default () => <Link to="/">Home</Link>;
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Helmet from "react-helmet";
+import Nav from "../components/Nav";
 import config from "../../data/SiteConfig";
-import "./index.css";
+import s from "./styles.module.scss";
 
-export default class MainLayout extends React.Component {
-  render() {
-    const { children } = this.props;
-    return (
-      <div>
-        <Helmet>
-          <meta name="description" content={config.siteDescription} />
-        </Helmet>
-        {children}
-      </div>
-    );
-  }
-}
+const MainLayout = ({ children }) => (
+  <div>
+    <Helmet>
+      <meta name="description" content={config.siteDescription} />
+    </Helmet>
+    <div className={s.container}>
+      <Nav />
+      {children}
+    </div>
+  </div>
+);
+
+export default MainLayout;
