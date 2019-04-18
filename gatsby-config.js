c1631469e2f65bafe82bb43fe05f2ad785f2e0bd
Wei Gao <wgao19@gmail.com>
2019-04-19 04:33:38 +0800

    Use generated favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -105,16 +105,41 @@ module.exports = {
         display: "minimal-ui",
         icons: [
           {
-            src: "/logos/logo-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/logos/logo-512x512.png",
-            sizes: "512x512",
+            src: "/logos/logo-64x64.png",
+            sizes: "64x64",
             type: "image/png"
           }
         ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./static/logos/logo-64x64.png",
+        // WebApp Manifest Configuration
+        appName: null, // Inferred with your package.json
+        appDescription: null,
+        developerName: null,
+        developerURL: null,
+        dir: "auto",
+        lang: "en-US",
+        background: "#fff",
+        theme_color: "#fff",
+        display: "standalone",
+        orientation: "any",
+        start_url: "/?homescreen=1",
+        version: "1.0",
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          yandex: false,
+          windows: false
+        }
       }
     },
 
