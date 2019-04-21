c358fb06eb68ddd62d9d437e266e6c5703d069d8
Wei Gao <wgao19@gmail.com>
2019-04-22 00:32:40 +0800

    Use typography theme

--- src/components/PostListing/PostListing.jsx
+++ src/components/PostListing/PostListing.jsx
@@ -17,15 +17,19 @@ class PostListing extends React.Component {
     });
     return postList;
   }
+
   render() {
     const postList = this.getPostList();
     return (
       <div>
         {/* Your post list here. */
         postList.map(post => (
-          <Link to={post.path} key={post.title}>
-            <h1>{post.title}</h1>
-          </Link>
+          <div key={post.title}>
+            <Link to={post.path}>
+              <h1>{post.title}</h1>
+            </Link>
+            <p>{post.excerpt}</p>
+          </div>
         ))}
       </div>
     );
---
+++ src/utils/typography.js
@@ -0,0 +1,4 @@
+import Typography from "typography";
+import themeElkGlen from "typography-theme-elk-glen";
+
+export default new Typography(themeElkGlen);
