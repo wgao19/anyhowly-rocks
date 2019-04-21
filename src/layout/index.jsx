3ad4c4388c63b51c420654531f14af5ad7b210e1
Wei Gao <wgao19@gmail.com>
2019-04-21 23:35:59 +0800

    Styling with sass and css modules

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -3,6 +3,7 @@ import { Link } from "gatsby";
 import Helmet from "react-helmet";
 import Intro from "../components/Intro";
 import config from "../../data/SiteConfig";
+import s from "./s.module.scss";
 
 export default class MainLayout extends React.Component {
   render() {
@@ -12,11 +13,13 @@ export default class MainLayout extends React.Component {
         <Helmet>
           <meta name="description" content={config.siteDescription} />
         </Helmet>
-        <h1>
-          <Link to="/">{config.siteTitle}</Link>
-        </h1>
-        <Intro meta={config} />
-        {children}
+        <div className={s.container}>
+          <h1 className={s.siteHeader}>
+            <Link to="/">{config.siteTitle}</Link>
+          </h1>
+          <Intro meta={config} />
+          {children}
+        </div>
       </div>
     );
   }
