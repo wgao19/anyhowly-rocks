f1e223aa5c6cbb4e2634f8d341e6677437e9a0e1
Wei Gao <wgao19@gmail.com>
2019-04-22 00:33:17 +0800

    Customize typographyjs theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,4 +1,11 @@
 import Typography from "typography";
 import themeElkGlen from "typography-theme-elk-glen";
 
+themeElkGlen.overrideThemeStyles = ({ rhythm }, options) => ({
+  h1: {
+    marginBottom: rhythm(1 / 2),
+    marginTop: rhythm(2)
+  }
+});
+
 export default new Typography(themeElkGlen);
